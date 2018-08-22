9c648b3b7dafd3f50d0433a9949cbf13b966e32d
Jonathan C Chik <Jonathan.Chik@ey.com>
2018-08-22 11:23:10 -0400

    post and user tables added, setup database

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,27 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-	user = {'username':'Miguel'}
-	return render_template('index.html', title='Home', user=user)
+	user = {'username':'Jonathan'}
+	posts = [
+		{
+			'author': {'username':'John'},
+			'body': 'Beautiful day in Portland!'
+		},
+		{
+			'author': {'username': 'Susan'},
+			'body': 'The Avengers moview as so cool!'
+		}]
+	return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, 
+        	form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
